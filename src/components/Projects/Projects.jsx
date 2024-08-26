9c34f6cb00bcda08322b85b21290fa199acd83e0
SouptikDatta <souptik13@gmail.com>
2024-08-26 15:02:38 +0530

    Redirects file for netlify error

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -33,7 +33,7 @@ const Portfolio = ( ) => {
 
 	return (
 		<div id='projects' className='projects'>
-			<section id="Projects" className="portWrap mt-10 py-[4.5rem] md:py-[5.5rem] lg:py-[6.5rem] xl:py-[7.5rem relative w-full">
+			<section id="Projects" className="portWrap mt-16 md:mt-0 py-[4.5rem] md:py-[5.5rem] lg:py-[6.5rem] xl:py-[7.5rem relative w-full">
 				<div
 					className="absolute left-0 top-20 bottom-0 -z-10 w-[100%] bg-left-top bg-no-repeat opacity-20"
 					style={{ backgroundImage: `url(${PatternImg})` }}
@@ -51,24 +51,24 @@ const Portfolio = ( ) => {
 										<div className="portBox group mb-[1.875rem] before:transition-all before:ease-in-out before:duration-500 before:bg-accent before:absolute before:opacity-0 before:hover:opacity-90 before:inset-[10%] before:hover:inset-0 relative overflow-hidden rounded-[10px] sm:rounded-[1.25rem] lg:rounded-[1.25rem] xl:rounded-[1.5rem] before:z-[1] text-center w-full">
 											<img className="w-full" src={item.image.url} alt={item.title}></img>
 											<div className="projInfo transition-all ease-in-out duration-500 delay-200 absolute left-0 group-hover:top-2/4 top-3/4 translate-y-[-50%] p-5 text-center w-full z-[1] opacity-0 invisible group-hover:opacity-100 group-hover:visible">
-												<h3 className="text-white text-xl md:text-[1.5rem] font-Poppins font-bold">{item.title}</h3>
-												<p className="text-white font-NunitoSans text-[1rem] md:text-[1rem] mt-2">{item.description}</p>
+												<h3 className="text-white text-sm md:text-[1.5rem] font-Poppins font-bold">{item.title}</h3>
+												<p className="text-white font-NunitoSans text-[0.7rem] md:text-[1rem] mt-1 md:mt-2">{item.description}</p>
 												<Link
-													className="inline-flex items-center justify-center border-[2px] sm:border-[5px] border-[rgba(255,255,255,.25)] hover:rotate-[-90deg] hover:duration-500 hover:border-white rounded-[10px] h-[2.5rem] w-[2.5rem] sm:h-[3.5625rem] sm:w-[3.5625rem] mt-5"
+													className="inline-flex items-center justify-center border-[2px] sm:border-[5px] border-[rgba(255,255,255,.25)] hover:rotate-[-90deg] hover:duration-500 hover:border-white rounded-[10px] sm:h-[3.5625rem] sm:w-[3.5625rem] h-[2.5rem] w-[2.5rem] mt-2 md:mt-5"
 													to={`/projects/${index+1}`}
 												>
-													<RightDownIcon className="h-2 w-2 sm:h-4 sm:w-4 fill-white"></RightDownIcon>
+													<RightDownIcon className="h-3 w-3 sm:h-4 sm:w-4 fill-white"></RightDownIcon>
 												</Link>
 											</div>
 										</div>
-										{/* Portfolio Box */}
+										{/* project Box */}
 									</div>
 								)
 						)}
-						{/* Portfolio List */}
+						{/* projects List */}
 					</div>
 				</div>
-				{/* Portfolio Wrap */}
+				{/* projects Wrap */}
 			</section>
 		</div>
 	);
